0631e9ce222a264b26ecea17c1a243836e725e36
David Bomba <turbo124@gmail.com>
2017-09-28 15:33:18 +1000

    transaction reference

--- app/Ninja/PaymentDrivers/PagSeguroPaymentDriver.php
+++ app/Ninja/PaymentDrivers/PagSeguroPaymentDriver.php
@@ -5,4 +5,15 @@ namespace App\Ninja\PaymentDrivers;
 class PagSeguroPaymentDriver extends BasePaymentDriver
 {
     protected $transactionReferenceParam = 'transactionReference';
+
+
+    protected function paymentDetails($paymentMethod = false)
+    {
+        $data = parent::paymentDetails($paymentMethod);
+        $data['transactionReference'] = $this->invoice()->invoice_number;
+
+        return $data;
+    }
+
+
 }
