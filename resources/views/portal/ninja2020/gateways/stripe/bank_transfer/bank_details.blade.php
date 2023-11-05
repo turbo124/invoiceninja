c783a4f4d4d7333bf1e76f748b9dda50816daa2d
David Bomba <turbo124@gmail.com>
2023-11-05 19:43:51 +1100

    Updates

--- resources/views/portal/ninja2020/gateways/stripe/bank_transfer/bank_details.blade.php
+++ resources/views/portal/ninja2020/gateways/stripe/bank_transfer/bank_details.blade.php
@@ -9,304 +9,306 @@
         <div class="px-4 py-5 bg-white sm:grid sm:grid-cols-1 sm:gap-4 sm:px-6">
 
         @if(isset($bank_details['currency']) && $bank_details['currency'] == 'gbp')
-
-            <dt class="text-sm font-medium leading-5 text-gray-500">
+        <dl class="grid grid-cols-1 md:grid-cols-2 gap-4">
+            <dt class="text-sm font-medium  text-gray-500">
                 {{ ctrans('texts.sort') }}
             </dt>
-            <dd class="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
+            <dd class="text-sm text-gray-900">
                 {{ $bank_details['sort_code'] }}
             </dd>
             
-            <dt class="text-sm font-medium leading-5 text-gray-500">
+            <dt class="text-sm font-medium  text-gray-500">
                 {{ ctrans('texts.account_number') }}
             </dt>
-            <dd class="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
+            <dd class="text-sm text-gray-900">
                 {{ $bank_details['account_number'] }}
             </dd>
 
-            <dt class="text-sm font-medium leading-5 text-gray-500">
+            <dt class="text-sm font-medium  text-gray-500">
                 {{ ctrans('texts.account_name') }}
             </dt>
-            <dd class="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
+            <dd class="text-sm text-gray-900">
                 {{ $bank_details['account_holder_name'] }}
             </dd>
 
             
-            <dt class="text-sm font-medium leading-5 text-gray-500">
+            <dt class="text-sm font-medium  text-gray-500">
                 {{ ctrans('texts.reference') }}
             </dt>
-            <dd class="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
+            <dd class="text-sm text-gray-900">
                 {{ $bank_details['reference'] }}
             </dd>
 
 
-            <dt class="text-sm font-medium leading-5 text-gray-500">
+            <dt class="text-sm font-medium  text-gray-500">
                 {{ ctrans('texts.balance_due') }}
             </dt>
-            <dd class="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
+            <dd class="text-sm text-gray-900">
                 {{ $bank_details['amount'] }}
             </dd>
 
-            <dt class="text-sm font-medium leading-5 text-gray-500">
+            <dt class="text-sm font-medium  text-gray-500">
             </dt>
-            <dd class="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
+            <dd class="text-sm text-gray-900">
                 {{ ctrans('texts.stripe_direct_debit_details') }}
             </dd>
-            
+        </dl>
             @elseif(isset($bank_details['currency']) && $bank_details['currency'] == 'mxn')
-            
-            <dt class="text-sm font-medium leading-5 text-gray-500">
+             <dl class="grid grid-cols-1 md:grid-cols-2 gap-4">
+            <dt class="text-sm font-medium  text-gray-500">
                 Clabe
             </dt>
-            <dd class="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
+            <dd class="text-sm text-gray-900">
                 {{ $bank_details['sort_code'] }}
             </dd>
 
-            <dt class="text-sm font-medium leading-5 text-gray-500">
+            <dt class="text-sm font-medium  text-gray-500">
                 {{ ctrans('texts.account_number') }}
             </dt>
-            <dd class="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
+            <dd class="text-sm text-gray-900">
                 {{ $bank_details['account_number'] }}
             </dd>
 
-            <dt class="text-sm font-medium leading-5 text-gray-500">
+            <dt class="text-sm font-medium  text-gray-500">
                 {{ ctrans('texts.account_name') }}
             </dt>
-            <dd class="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
+            <dd class="text-sm text-gray-900">
                 {{ $bank_details['account_holder_name'] }}
             </dd>
 
 
-            <dt class="text-sm font-medium leading-5 text-gray-500">
+            <dt class="text-sm font-medium  text-gray-500">
                 {{ ctrans('texts.reference') }}
             </dt>
-            <dd class="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
+            <dd class="text-sm text-gray-900">
                 {{ $bank_details['reference'] }}
             </dd>
 
 
-            <dt class="text-sm font-medium leading-5 text-gray-500">
+            <dt class="text-sm font-medium  text-gray-500">
                 {{ ctrans('texts.balance_due') }}
             </dt>
-            <dd class="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
+            <dd class="text-sm text-gray-900">
                 {{ $bank_details['amount'] }}
             </dd>
 
-            <dt class="text-sm font-medium leading-5 text-gray-500">
+            <dt class="text-sm font-medium  text-gray-500">
             </dt>
-            <dd class="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
+            <dd class="text-sm text-gray-900">
                 {{ ctrans('texts.stripe_direct_debit_details') }}
             </dd>
             
             @elseif(isset($bank_details['currency']) && $bank_details['currency'] == 'jpy')
             
-            <dt class="text-sm font-medium leading-5 text-gray-500">
+            <dt class="text-sm font-medium  text-gray-500">
                 {{ ctrans('texts.account_number') }}
             </dt>
-            <dd class="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
+            <dd class="text-sm text-gray-900">
                 {{ $bank_details['account_number'] }}
             </dd>
 
-            <dt class="text-sm font-medium leading-5 text-gray-500">
+            <dt class="text-sm font-medium  text-gray-500">
                 {{ ctrans('texts.account_name') }}
             </dt>
-            <dd class="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
+            <dd class="text-sm text-gray-900">
                 {{ $bank_details['account_holder_name'] }}
             </dd>
 
-            <dt class="text-sm font-medium leading-5 text-gray-500">
+            <dt class="text-sm font-medium  text-gray-500">
                 {{ ctrans('texts.account_type') }}
             </dt>
-            <dd class="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
+            <dd class="text-sm text-gray-900">
                 {{ $bank_details['account_type'] }}
             </dd>
 
-            <dt class="text-sm font-medium leading-5 text-gray-500">
+            <dt class="text-sm font-medium  text-gray-500">
                 {{ ctrans('texts.bank_name') }}
             </dt>
-            <dd class="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
+            <dd class="text-sm text-gray-900">
                 {{ $bank_details['bank_name'] }}
             </dd>
 
-            <dt class="text-sm font-medium leading-5 text-gray-500">
+            <dt class="text-sm font-medium  text-gray-500">
                 {{ ctrans('texts.bank_code') }}
             </dt>
-            <dd class="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
+            <dd class="text-sm text-gray-900">
                 {{ $bank_details['bank_code'] }}
             </dd>
 
-            <dt class="text-sm font-medium leading-5 text-gray-500">
+            <dt class="text-sm font-medium  text-gray-500">
                 {{ ctrans('texts.branch_name') }}
             </dt>
-            <dd class="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
+            <dd class="text-sm text-gray-900">
                 {{ $bank_details['branch_name'] }}
             </dd>
 
-            <dt class="text-sm font-medium leading-5 text-gray-500">
+            <dt class="text-sm font-medium  text-gray-500">
                 {{ ctrans('texts.branch_code') }}
             </dt>
-            <dd class="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
+            <dd class="text-sm text-gray-900">
                 {{ $bank_details['branch_code'] }}
             </dd>
 
-            <dt class="text-sm font-medium leading-5 text-gray-500">
+            <dt class="text-sm font-medium  text-gray-500">
                 {{ ctrans('texts.reference') }}
             </dt>
-            <dd class="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
+            <dd class="text-sm text-gray-900">
                 {{ $bank_details['reference'] }}
             </dd>
 
 
-            <dt class="text-sm font-medium leading-5 text-gray-500">
+            <dt class="text-sm font-medium  text-gray-500">
                 {{ ctrans('texts.balance_due') }}
             </dt>
-            <dd class="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
+            <dd class="text-sm text-gray-900">
                 {{ $bank_details['amount'] }}
             </dd>
 
-            <dt class="text-sm font-medium leading-5 text-gray-500">
+            <dt class="text-sm font-medium  text-gray-500">
             </dt>
-            <dd class="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
+            <dd class="text-sm text-gray-900">
                 {{ ctrans('texts.stripe_direct_debit_details') }}
             </dd>
-
+             </dl>
         @elseif(isset($bank_details['currency']) && $bank_details['currency'] == 'eur')
 
-            <dt class="text-sm font-medium leading-5 text-gray-500">
+            <dt class="text-sm font-medium  text-gray-500">
                     {{ ctrans('texts.account_name') }}
                 </dt>
-                <dd class="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
+                <dd class="text-sm text-gray-900">
                     {{ $bank_details['account_holder_name'] }}
                 </dd>
 
-                <dt class="text-sm font-medium leading-5 text-gray-500">
+                <dt class="text-sm font-medium  text-gray-500">
                     {{ ctrans('texts.account_number') }}
                 </dt>
-                <dd class="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
+                <dd class="text-sm text-gray-900">
                     {{ $bank_details['account_number'] }}
                 </dd>
 
-                <dt class="text-sm font-medium leading-5 text-gray-500">
+                <dt class="text-sm font-medium  text-gray-500">
                     {{ ctrans('texts.bic') }}
                 </dt>
-                <dd class="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
+                <dd class="text-sm text-gray-900">
                     {{ $bank_details['sort_code'] }}
                 </dd>
 
-                <dt class="text-sm font-medium leading-5 text-gray-500">
+                <dt class="text-sm font-medium  text-gray-500">
                     {{ ctrans('texts.reference') }}
                 </dt>
-                <dd class="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
+                <dd class="text-sm text-gray-900">
                     {{ $bank_details['reference'] }}
                 </dd>
 
 
-                <dt class="text-sm font-medium leading-5 text-gray-500">
+                <dt class="text-sm font-medium  text-gray-500">
                     {{ ctrans('texts.balance_due') }}
                 </dt>
-                <dd class="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
+                <dd class="text-sm text-gray-900">
                     {{ $bank_details['amount'] }}
                 </dd>
 
-                <dt class="text-sm font-medium leading-5 text-gray-500">
+                <dt class="text-sm font-medium  text-gray-500">
                 </dt>
-                <dd class="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
+                <dd class="text-sm text-gray-900">
                     {{ ctrans('texts.stripe_direct_debit_details') }}
                 </dd>
 
         @elseif(isset($bank_details['aba_details']) || isset($bank_details['swift_details']))
 
-            <div class="grid grid-cols-2 gap-4">
-            @isset($bank_details['aba_details'])
-            <div class="whitespace-nowrap">
-            <dt class="text-sm font-medium leading-5 text-gray-500">
+        <div class="grid grid-cols-2 gap-4">
+        @isset($bank_details['aba_details'])
+            <dl class="grid grid-cols-2 gap-2 p-2">
+                <dt class="text-sm font-medium text-gray-500">
                     {{ ctrans('texts.bank_name') }}
                 </dt>
-                <dd class="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
+                <dd class="text-sm text-gray-900">
                     {{ $bank_details['aba_details']['bank_name'] }}
                 </dd>
 
-                <dt class="text-sm font-medium leading-5 text-gray-500">
+                <dt class="text-sm font-medium  text-gray-500">
                     {{ ctrans('texts.account_number') }}
                 </dt>
-                <dd class="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
+
+                <dd class="text-sm text-gray-900">
                     {{ $bank_details['aba_details']['account_number'] }}
                 </dd>
 
-                <dt class="text-sm font-medium leading-5 text-gray-500">
+                <dt class="text-sm font-medium  text-gray-500">
                     {{ ctrans('texts.routing_number') }}
                 </dt>
-                <dd class="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
+                <dd class="text-sm text-gray-900">
                     {{ $bank_details['aba_details']['routing_number'] }}
                 </dd>
 
-                <dt class="text-sm font-medium leading-5 text-gray-500">
+                <dt class="text-sm font-medium  text-gray-500">
                     {{ ctrans('texts.reference') }}
                 </dt>
-                <dd class="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
+                <dd class="text-sm text-gray-900">
                     {{ $bank_details['aba_details']['reference'] }}
                 </dd>
 
-
-                <dt class="text-sm font-medium leading-5 text-gray-500">
+                <dt class="text-sm font-medium  text-gray-500">
                     {{ ctrans('texts.balance_due') }}
                 </dt>
-                <dd class="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
+                <dd class="text-sm text-gray-900">
                     {{ $bank_details['aba_details']['amount'] }}
                 </dd>
 
-                <dt class="text-sm font-medium leading-5 text-gray-500">
+                <dt class="text-sm font-medium  text-gray-500">
                 </dt>
-                <dd class="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
+                <dd class="text-sm text-gray-900">
                     {{ ctrans('texts.stripe_direct_debit_details') }}
                 </dd>
-            </div>
+            
+            </dl>
+    
             @endisset
 
             @isset($bank_details['swift_details'])
-            <div class="whitespace-nowrap">
-            <dt class="text-sm font-medium leading-5 text-gray-500 whitespace-nowrap">
+            <dl class="grid grid-cols-2 gap-2 p-2">
+                <dt class="text-sm font-medium text-gray-500">
                     {{ ctrans('texts.bank_name') }}
                 </dt>
-                <dd class="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2 whitespace-nowrap">
+                <dd class="text-sm text-gray-900">
                     {{ $bank_details['swift_details']['bank_name'] }}
                 </dd>
 
-                <dt class="text-sm font-medium leading-5 text-gray-500">
+                <dt class="text-sm font-medium  text-gray-500">
                     {{ ctrans('texts.account_number') }}
                 </dt>
-                <dd class="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
+                <dd class="text-sm text-gray-900">
                     {{ $bank_details['swift_details']['account_number'] }}
                 </dd>
 
-                <dt class="text-sm font-medium leading-5 text-gray-500">
+                <dt class="text-sm font-medium  text-gray-500">
                     {{ ctrans('texts.swift_code') }}
                 </dt>
-                <dd class="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
+                <dd class="text-sm text-gray-900">
                     {{ $bank_details['swift_details']['swift_code'] }}
                 </dd>
 
-                <dt class="text-sm font-medium leading-5 text-gray-500">
+                <dt class="text-sm font-medium  text-gray-500">
                     {{ ctrans('texts.reference') }}
                 </dt>
-                <dd class="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
+                <dd class="text-sm text-gray-900">
                     {{ $bank_details['swift_details']['reference'] }}
                 </dd>
 
 
-                <dt class="text-sm font-medium leading-5 text-gray-500">
+                <dt class="text-sm font-medium  text-gray-500">
                     {{ ctrans('texts.balance_due') }}
                 </dt>
-                <dd class="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
+                <dd class="text-sm text-gray-900">
                     {{ $bank_details['swift_details']['amount'] }}
                 </dd>
 
-                <dt class="text-sm font-medium leading-5 text-gray-500">
+                <dt class="text-sm font-medium  text-gray-500">
                 </dt>
-                <dd class="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
+                <dd class="text-sm text-gray-900">
                     {{ ctrans('texts.stripe_direct_debit_details') }}
                 </dd>
-            </div>
+            </dl>
             @endisset
             </div>
             @endif
