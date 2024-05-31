2d3910582fc5218e449a7d1c0bfa38f83b9b4d46
Shift <shift@laravelshift.com>
2024-05-31 22:01:12 +0000

    Re-register service providers

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,4 +2,10 @@
 
 return [
     App\Providers\AppServiceProvider::class,
+    App\Providers\EventServiceProvider::class,
+    App\Providers\RouteServiceProvider::class,
+    App\Providers\ComposerServiceProvider::class,
+    App\Providers\MultiDBProvider::class,
+    App\Providers\ClientPortalServiceProvider::class,
+    App\Providers\NinjaTranslationServiceProvider::class,
 ];
--- config/app.php
+++ config/app.php
@@ -7,32 +7,6 @@ return [
 
     'mix_url' => env('MIX_ASSET_URL', env('APP_URL', null)),
 
-    'providers' => ServiceProvider::defaultProviders()->merge([
-        /*
-         * Laravel Framework Service Providers...
-         */
-        /*
-         * Dependency Service Providers
-         */
-        'Webpatser\Countries\CountriesServiceProvider',
-
-        /*
-         * Package Service Providers...
-         */
-
-        /*
-         * Application Service Providers...
-         */
-        App\Providers\AppServiceProvider::class,
-        App\Providers\AuthServiceProvider::class,
-        // App\Providers\BroadcastServiceProvider::class,
-        App\Providers\EventServiceProvider::class,
-        App\Providers\RouteServiceProvider::class,
-        App\Providers\ComposerServiceProvider::class,
-        App\Providers\MultiDBProvider::class,
-        App\Providers\ClientPortalServiceProvider::class,
-        App\Providers\NinjaTranslationServiceProvider::class,
-    ])->toArray(),
 
     'aliases' => Facade::defaultAliases()->merge([
         'Collector' => Turbo124\Collector\CollectorFacade::class,
