d09b2ed8d648ee0022f42e47ed7c934013018801
Benjamin Beganović <k1pstabug@gmail.com>
2024-07-16 19:41:39 +0200

    authorize.net

--- resources/js/clients/payments/authorize-credit-card-payment.js
+++ resources/js/clients/payments/authorize-credit-card-payment.js
@@ -5,26 +5,31 @@
  *
  * @copyright Copyright (c) 2021. Invoice Ninja LLC (https://invoiceninja.com)
  *
- * @license https://www.elastic.co/licensing/elastic-license 
+ * @license https://www.elastic.co/licensing/elastic-license
  */
 
-class AuthorizeAuthorizeCard {
+import { wait, instant } from '../wait';
 
+class AuthorizeAuthorizeCard {
     constructor(publicKey, loginId) {
         this.publicKey = publicKey;
         this.loginId = loginId;
-        this.cardHolderName = document.getElementById("cardholder_name");
+        this.cardHolderName = document.getElementById('cardholder_name');
     }
 
     handleAuthorization = () => {
-
-        if (cvvRequired == "1" && document.getElementById("cvv").value.length < 3) {
+        if (
+            cvvRequired == '1' &&
+            document.getElementById('cvv').value.length < 3
+        ) {
             var $errors = $('#errors');
-            $errors.show().html("<p>CVV is required</p>");
+            $errors.show().html('<p>CVV is required</p>');
 
             document.getElementById('pay-now').disabled = false;
             document.querySelector('#pay-now > svg').classList.add('hidden');
-            document.querySelector('#pay-now > span').classList.remove('hidden');
+            document
+                .querySelector('#pay-now > span')
+                .classList.remove('hidden');
             return;
         }
 
@@ -38,7 +43,9 @@ class AuthorizeAuthorizeCard {
         cardData.cardNumber = myCard.CardJs('cardNumber').replace(/[^\d]/g, '');
         cardData.month = myCard.CardJs('expiryMonth').replace(/[^\d]/g, '');
         cardData.year = myCard.CardJs('expiryYear').replace(/[^\d]/g, '');
-        cardData.cardCode = document.getElementById("cvv").value.replace(/[^\d]/g, '');
+        cardData.cardCode = document
+            .getElementById('cvv')
+            .value.replace(/[^\d]/g, '');
 
         var secureData = {};
         secureData.authData = authData;
@@ -58,102 +65,153 @@ class AuthorizeAuthorizeCard {
         Accept.dispatchData(secureData, this.responseHandler);
 
         return false;
-
-    }
+    };
 
     handlePayNowAction(token_hashed_id) {
         document.getElementById('pay-now').disabled = true;
         document.querySelector('#pay-now > svg').classList.remove('hidden');
         document.querySelector('#pay-now > span').classList.add('hidden');
 
-        document.getElementById("token").value = token_hashed_id;
-        document.getElementById("server_response").submit();
+        document.getElementById('token').value = token_hashed_id;
+        document.getElementById('server_response').submit();
     }
 
     responseHandler = (response) => {
-        if (response.messages.resultCode === "Error") {
+        if (response.messages.resultCode === 'Error') {
             var i = 0;
 
             var $errors = $('#errors'); // get the reference of the div
-            $errors.show().html("<p>" + response.messages.message[i].code + ": " + response.messages.message[i].text + "</p>");
+            $errors
+                .show()
+                .html(
+                    '<p>' +
+                        response.messages.message[i].code +
+                        ': ' +
+                        response.messages.message[i].text +
+                        '</p>'
+                );
 
             document.getElementById('pay-now').disabled = false;
             document.querySelector('#pay-now > svg').classList.add('hidden');
-            document.querySelector('#pay-now > span').classList.remove('hidden');
-        } else if (response.messages.resultCode === "Ok") {
+            document
+                .querySelector('#pay-now > span')
+                .classList.remove('hidden');
+        } else if (response.messages.resultCode === 'Ok') {
+            document.getElementById('dataDescriptor').value =
+                response.opaqueData.dataDescriptor;
+            document.getElementById('dataValue').value =
+                response.opaqueData.dataValue;
 
-            document.getElementById("dataDescriptor").value = response.opaqueData.dataDescriptor;
-            document.getElementById("dataValue").value = response.opaqueData.dataValue;
-
-            let storeCard = document.querySelector('input[name=token-billing-checkbox]:checked');
+            let storeCard = document.querySelector(
+                'input[name=token-billing-checkbox]:checked'
+            );
 
             if (storeCard) {
-                document.getElementById("store_card").value = storeCard.value;
+                document.getElementById('store_card').value = storeCard.value;
             }
 
-            document.getElementById("server_response").submit();
+            document.getElementById('server_response').submit();
         }
 
         return false;
-    }
-
+    };
 
     handle = () => {
-        Array
-            .from(document.getElementsByClassName('toggle-payment-with-token'))
-            .forEach((element) => element.addEventListener('click', (e) => {
-                document
-                    .getElementById('save-card--container').style.display = 'none';
-                document
-                    .getElementById('authorize--credit-card-container').style.display = 'none';
+        Array.from(
+            document.getElementsByClassName('toggle-payment-with-token')
+        ).forEach((element) =>
+            element.addEventListener('click', (e) => {
+                document.getElementById('save-card--container').style.display =
+                    'none';
+                document.getElementById(
+                    'authorize--credit-card-container'
+                ).style.display = 'none';
 
-                document
-                    .getElementById('token').value = e.target.dataset.token;
-            }));
+                document.getElementById('token').value = e.target.dataset.token;
+            })
+        );
 
-        let payWithCreditCardToggle = document.getElementById('toggle-payment-with-credit-card');
+        let payWithCreditCardToggle = document.getElementById(
+            'toggle-payment-with-credit-card'
+        );
 
         if (payWithCreditCardToggle) {
-            payWithCreditCardToggle
-                .addEventListener('click', () => {
-                    document
-                        .getElementById('save-card--container').style.display = 'grid';
-                    document
-                        .getElementById('authorize--credit-card-container').style.display = 'flex';
+            payWithCreditCardToggle.addEventListener('click', () => {
+                document.getElementById('save-card--container').style.display =
+                    'grid';
+                document.getElementById(
+                    'authorize--credit-card-container'
+                ).style.display = 'flex';
 
-                    document
-                        .getElementById('token').value = null;
-                });
+                document.getElementById('token').value = null;
+            });
         }
 
         let payNowButton = document.getElementById('pay-now');
 
         if (payNowButton) {
-            payNowButton
-                .addEventListener('click', (e) => {
-                    let token = document.getElementById('token');
+            payNowButton.addEventListener('click', (e) => {
+                let token = document.getElementById('token');
 
-                    token.value
-                        ? this.handlePayNowAction(token.value)
-                        : this.handleAuthorization();
-                });
+                token.value
+                    ? this.handlePayNowAction(token.value)
+                    : this.handleAuthorization();
+            });
         }
 
         return this;
-    }
+    };
 }
 
-const publicKey = document.querySelector(
-    'meta[name="authorize-public-key"]'
-).content;
+function boot() {
+    // The following code was showing syntax error:
 
-const loginId = document.querySelector(
-    'meta[name="authorize-login-id"]'
-).content;
+    // document
+    //     .getElementsByClassName('expiry')[0]
+    //     .addEventListener('change', function () {
+    //         str = document
+    //             .getElementsByClassName('expiry')[0]
+    //             .value.replace(/\s/g, '');
+    //         const expiryArray = str.split('/');
 
-const cvvRequired = document.querySelector(
-    'meta[name="authnet-require-cvv"]'
-).content;
+    //         document.getElementsByName('expiry-month')[0].value =
+    //             expiryArray[0];
+    //         document.getElementsByName('expiry-year')[0].value = expiryArray[1];
+    //     });
 
-/** @handle */
-new AuthorizeAuthorizeCard(publicKey, loginId).handle();
+    console.log('boot');
+
+    const publicKey = document.querySelector(
+        'meta[name="authorize-public-key"]'
+    ).content;
+
+    const loginId = document.querySelector(
+        'meta[name="authorize-login-id"]'
+    ).content;
+
+    const cvvRequired = document.querySelector(
+        'meta[name="authnet-require-cvv"]'
+    ).content;
+
+    /** @handle */
+    new AuthorizeAuthorizeCard(publicKey, loginId).handle();
+}
+
+if (instant()) {
+    boot();
+} else {
+    wait('#authorize-net-credit-card-payment', 'meta[name='card-js-url']').then(() => {
+        const jquery = document.createElement('script');
+
+        jquery.src = 'https://code.jquery.com/jquery-1.11.3.min.js';
+    
+        const cardjs = document.createElement('script');
+    
+        cardjs.src = document.querySelector("meta[name='card-js-url']").content;
+    
+        document.body.appendChild(jquery);
+        document.body.appendChild(cardjs);
+    
+        boot();
+    })
+}
