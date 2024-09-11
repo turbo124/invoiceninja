2bc443f7c6cd6c79eb7c224ecd66589a683c5448
Benjamin Beganović <k1pstabug@gmail.com>
2024-09-11 12:45:57 +0200

    update vite refs

--- vite.config.ts
+++ vite.config.ts
@@ -54,7 +54,7 @@ export default defineConfig({
             'resources/js/clients/payments/stripe-bank-transfer.js',
             'resources/js/clients/payment_methods/authorize-stripe-acss.js',
             'resources/js/clients/payment_methods/authorize-powerboard-card.js',
-            'resources/js/clients/payment_methods/authorize-powerboard-card.no3ds.js',
+            'resources/js/clients/payments/powerboard-credit-card.js',
         ]),
         viteStaticCopy({
             targets: [
