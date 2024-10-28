657239c840f247165250e67a76db4c7ff85b357f
Benjamin Beganović <k1pstabug@gmail.com>
2024-10-28 16:22:22 +0100

    remove references of e_invoicing_token from company

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -362,7 +362,6 @@ class Company extends BaseModel
         'smtp_local_domain',
         'smtp_verify_peer',
         // 'e_invoice',
-        'e_invoicing_token',
     ];
 
     protected $hidden = [
