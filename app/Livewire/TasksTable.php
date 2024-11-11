ee32dab712de6b73588f9bf4d9a07df1ccf6c8d5
David Bomba <turbo124@gmail.com>
2024-11-11 17:55:51 +1100

    Fixes for displaying tasks in client portal

--- app/Livewire/TasksTable.php
+++ app/Livewire/TasksTable.php
@@ -54,7 +54,7 @@ class TasksTable extends Component
 
         return render('components.livewire.tasks-table', [
             'tasks' => $query,
-            'show_item_description' => auth()->guard('contact')->user()->company->invoice_task_item_description ?? false,
+            'show_item_description' => auth()->guard('contact')->user()->client->getSetting("show_task_item_description"),
         ]);
     }
 }
