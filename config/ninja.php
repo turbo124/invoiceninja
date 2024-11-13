60a7e073004bf87ece91d214c8244195907b19b8
Benjamin Beganović <k1pstabug@gmail.com>
2024-11-13 17:20:34 +0100

    E_INVOICE_QUOTA_WARNING

--- config/ninja.php
+++ config/ninja.php
@@ -259,4 +259,5 @@ return [
     'pdf_page_numbering_x_alignment' => env('PDF_PAGE_NUMBER_X', 0),
     'pdf_page_numbering_y_alignment' => env('PDF_PAGE_NUMBER_Y', -6),
     'hosted_einvoice_secret' => env('HOSTED_EINVOICE_SECRET', null),
+    'e_invoice_quota_warning' => env('E_INVOICE_QUOTA_WARNING', 15),
 ];
