02689f32349d2c1718d4c5378956f91753c6f257
David Bomba <turbo124@gmail.com>
2024-11-23 17:23:01 +1100

    Updates for quote reminders

--- app/Services/Quote/SendEmail.php
+++ app/Services/Quote/SendEmail.php
@@ -31,7 +31,7 @@ class SendEmail
     public function run()
     {
 
-        if(in_array($this->reminder_template, ["email_quote_template_reminder1","reminder1"]));
+        if(in_array($this->reminder_template, ["email_quote_template_reminder1","reminder1"]))
             $this->reminder_template = "email_quote_template_reminder1";
         else    
             $this->reminder_template = "email_template_".$this->quote->calculateTemplate('quote');
