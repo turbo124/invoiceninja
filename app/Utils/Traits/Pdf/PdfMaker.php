9180b7abb3141504ac425752cd09914d202732dd
David Bomba <turbo124@gmail.com>
2025-01-11 16:38:09 +1100

    Cleanup

--- app/Utils/Traits/Pdf/PdfMaker.php
+++ app/Utils/Traits/Pdf/PdfMaker.php
@@ -93,7 +93,7 @@ trait PdfMaker
             $pdf->setChromiumPath(config('ninja.snappdf_chromium_path'));
         }
 
-        // $html = str_ireplace(['file:/', 'iframe', '<embed', '&lt;embed', '&lt;object', '<object', '127.0.0.1', 'localhost'], '', $html);
+        $html = str_ireplace(['file:/', 'iframe', '<embed', '&lt;embed', '&lt;object', '<object', '127.0.0.1', 'localhost'], '', $html);
         
         // nlog($html);
         $generated = $pdf
