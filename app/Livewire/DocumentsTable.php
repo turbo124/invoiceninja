8de86a2abd668e61c6db0726fc961c79a969d37d
David Bomba <turbo124@gmail.com>
2025-01-20 08:41:05 +1100

    Fixes for client documents query

--- app/Livewire/DocumentsTable.php
+++ app/Livewire/DocumentsTable.php
@@ -122,7 +122,7 @@ class DocumentsTable extends Component
 
         return $client->documents()
             ->where('is_public', true)
-            ->orWhere(function ($query){
+            ->orWhere(function ($query) use ($client) {
                                 
                 $query->whereHasMorph('documentable', [Company::class], function ($q) use ($client) {
                     $q->where('is_public', true)->where('company_id', $client->company_id);
