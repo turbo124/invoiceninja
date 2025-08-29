1d599c980f4fbfb5dc827eabc896f9fca8bc59c8
Benjamin Beganović <k1pstabug@gmail.com>
2025-08-29 17:13:29 +0200

    Add docuninja_api_url config

--- config/ninja.php
+++ config/ninja.php
@@ -264,4 +264,5 @@ return [
     'disable_purify_html' => env('DISABLE_PURIFY_HTML', false),
     'zugferd_version_two' => env('ZUGFERD_VERSION_TWO', true),
     'chart_service_url' => env('CHART_SERVICE_URL', false),
+    'docuninja_api_url' => env('DOCUNINJA_API_URL', 'https://docuninja.co/api/'),
 ];
