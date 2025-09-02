eb80443f2d091f51faba279d831831319f7437cb
Benjamin Beganović <k1pstabug@gmail.com>
2025-09-02 18:20:31 +0200

    Update page to reflect events

--- resources/views/portal/ninja2020/flow2/docu-ninja.blade.php
+++ resources/views/portal/ninja2020/flow2/docu-ninja.blade.php
@@ -1,5 +1,6 @@
+<link rel="stylesheet" href="{{ asset('build/dist/builder2.0.standalone.css/builder2.0.standalone.css') }}">
+
 <div class="rounded-lg border bg-card text-card-foreground shadow-sm overflow-hidden px-4 py-5 bg-white sm:gap-4 sm:px-6">
-   
     <div class="p-2">
         @if($errors->any())
         <div class="alert alert-error">
@@ -18,15 +19,10 @@
     </div>
         
     <div id="sign"></div>
-
 </div>
     
-    
 @assets
-<link rel="stylesheet" href="/vendor/docuninja/builder2.0/dist/builder2.0.standalone.css" />
-<script src="/vendor/docuninja/builder2.0/dist/builder.iife.js"></script>
-
-<script>
+<script type="module">
     const token = '{{ $token }}';
     const doc = '{{ $document }}';
     const invitation = '{{ $invitation }}';
@@ -34,7 +30,10 @@
     
     const mount = document.getElementById("sign");
     
-    new DocuNinjaSign({ token, doc, invitation, sig }).mount(mount);
-</script>
+    new DocuNinjaSign({ token, document: doc, invitation, sig, endpoint: 'https:/docuninja.romulus.com.au' }).mount(mount);
 
+    window.addEventListener('builder:sign.submit.success', function () {
+        Livewire.dispatch('docuninja-signature-captured');
+    });
+</script>
 @endassets
